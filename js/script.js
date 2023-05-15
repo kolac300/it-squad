9a9305fac8f8666bd51f03553f31782030049fe1
KlimMelnik <113517514+KlimMelnik@users.noreply.github.com>
2023-05-15 16:20:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ const swiper = new Swiper('.swiper-default',
   
 const swiper2 = new Swiper('.swiper-style1', 
 {
-    loop: true,
+    loop: false,
     slidesPerView: 1,
     spaceBetween: 10,
     pagination: {
@@ -96,13 +96,89 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// const showElement = document.getElementById('portfolio-scroll');
-// const hiddenElement = document.getElementById('element-hidden-1');
+//Portfolio items
+let descriptions = {
+  vitalgym: {
+    images: [
+      'http://via.placeholder.com/640x360',
+      'http://via.placeholder.com/640x360',
+      'http://via.placeholder.com/640x360'
+    ],
+    name: "Vital Gym",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nisi id dolore porro nam nemo, omnis a sit architecto aut. Est quisquam voluptatem repudiandae, sequi aliquam quam reiciendis omnis consequuntur?",
+    date: "April 2019",
+    tech: "HTML5, SCSS, JavaScript",
+    url: "vitalgym.co"
+  },
+  mybacs: {
+    images: [
+      'https://loremflickr.com/640/360',
+      'https://loremflickr.com/640/360',
+      'https://loremflickr.com/640/360'
+    ],
+    name: "MyBacs",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nisi id dolore porro nam nemo, omnis a sit architecto aut. Est quisquam voluptatem repudiandae, sequi aliquam quam reiciendis omnis consequuntur?",
+    date: "April 2022",
+    tech: "HTML5, SCSS, JavaScript",
+    url: "mybacs.com"
+  },
+  beautytables: {
+    images: [
+      'https://placekitten.com/640/360',
+      'https://placekitten.com/640/360',
+      'https://placekitten.com/640/360'
+    ],
+    name: "Beauty Tables",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nisi id dolore porro nam nemo, omnis a sit architecto aut. Est quisquam voluptatem repudiandae, sequi aliquam quam reiciendis omnis consequuntur?",
+    date: "April 2022",
+    tech: "HTML5, SCSS, JavaScript",
+    url: "beautytables.co.uk"
+  }
+}
 
-// showElement.addEventListener('click', function() {
-//   if (hiddenElement.style.display == 'none') {
-//     hiddenElement.style.display = 'block';
-//   } else {
-//     hiddenElement.style.display = 'none';
-//   }
-// });
+let portfilio_items = document.querySelectorAll('.portfolio__item');
+let portfolio_description = document.querySelector('.portfolio__description-wraper');
+let close_description_btn = document.querySelector('.portfolio__btn-close');
+
+
+const updateDescriptionInfo = event => {
+  let description_images = document.querySelectorAll('.portfolio__description-image img');
+  let description_name = document.querySelector('.portfolio__description-title');
+  let description_info = document.querySelector('.portfolio__description-text_info');
+  let description_date = document.querySelector('.portfolio__description-text_date');
+  let description_url = document.querySelector('.portfolio__description-text_url');
+  let description_tech = document.querySelector('.portfolio__description-text_tech');
+  
+
+  let current_project = event.target.closest('.portfolio__item').dataset.project;
+  for (let [key, value] of Object.entries(descriptions)){
+    if (key == current_project){
+      //Update images
+      for (let i = 0; i < value.images.length; i++){
+        description_images[i].src = value.images[i];
+      }
+      //Update info
+      description_name.textContent = value.name;
+      description_info.textContent = value.description;
+      description_date.textContent = value.date;
+      description_url.textContent = value.url;
+      description_tech.textContent = value.tech;
+    }
+  }
+}
+
+const togglePortfolioDescription = event => {
+  if (event.target.closest('.portfolio__btn-close')){
+    portfolio_description.classList.add('_hidden');
+    document.querySelector('.portfolio__container').scrollIntoView({ behavior: 'smooth', block: "start" });
+    return ;
+  }else{
+    updateDescriptionInfo(event);
+    portfolio_description.classList.remove('_hidden');
+    portfolio_description.scrollIntoView({ behavior: 'smooth', block: "start" });
+  }
+}
+
+
+portfilio_items.forEach(el => el.addEventListener('click', togglePortfolioDescription));
+close_description_btn.addEventListener('click', togglePortfolioDescription)
